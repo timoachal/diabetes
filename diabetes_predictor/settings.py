2cb3d5ea518bfc6967dadeaa0927e4da56494ccd
Timothy Achala <145480498+timoachal@users.noreply.github.com>
2025-08-20 00:29:58 +0300

    Update settings.py

--- diabetes_predictor/settings.py
+++ diabetes_predictor/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8y)a_)_com1za9x_7&pdp!)iabat%stx9rf)@1-6kq+j(e09*@'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
